f5503debf55a219d164a6f25ff66033a86ec317f
Eyub <eyub.kh@gmail.com>
2021-05-27 08:06:43 +0100

    in to React Router

--- src/App.js
+++ src/App.js
@@ -2,47 +2,12 @@ import './App.css';
 import Header from './Header';
 import Card from './Cards';
 
-const cursos = [
-    {
-        title: 'HTML beginner',
-        teacher: 'Ayub',
-        img: 'https://www.000webhost.com/blog/wp-content/uploads/sites/15/2019/02/html-for-beginners.png',
-        price: 20,
-    },
-    {
-        title: 'JS beginner',
-        teacher: 'Isma',
-        img: 'https://images.unsplash.com/photo-1514565131-fce0801e5785?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2l0eXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-        price: 10,
-    },
-    {
-        title: 'React desde cero',
-        teacher: 'Isac',
-        img: 'https://img.freepik.com/free-photo/observation-urban-building-business-steel_1127-2397.jpg?size=626&ext=jpg',
-        price: 40,
-    },
-    {
-        title: 'React desde cero',
-        teacher: 'Isac',
-        img: 'https://img.freepik.com/free-photo/observation-urban-building-business-steel_1127-2397.jpg?size=626&ext=jpg',
-        price: 50,
-    },
-];
-
 function App() {
     return (
         <>
             <Header title="Probando react" />
             <main>
-                {cursos.map((curso, key) => (
-                    <Card
-                        key={key}
-                        title={curso.title}
-                        teacher={curso.teacher}
-                        img={curso.img}
-                        price={curso.price}
-                    />
-                ))}
+                <Card />
             </main>
         </>
     );
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,26 +1,61 @@
 import React from 'react';
 
 export default class Card extends React.Component {
-    constructor({title,teacher,img,price}){
-        super()
-        this.title = title;
-        this.teacher = teacher;
-        this.img = img;
-        this.price = price;
-    }
+    constructor(){
+        super();
+        this.state = {
+            name:  '.',
+            email: '.',
+            date: new Date().getSeconds()
+        };
+
+        this.clockSeconds = this.clockSeconds.bind(this)
+    };
+
+    changeState(i , e){
+        this.setState({
+            [i]: e.target.value
+        });
+    };
+
+    clockSeconds(){
+        this.setState({
+            date: new Date().getSeconds()
+        });
+    };
 
     render() {
         return (
-            <article key={this.key}>
-                <img src={this.img} alt="" />
-                <div className="card-info">
-                    <h2>{this.title}</h2>
-                    <p>{this.teacher}</p>
-                    <span>$ {this.price} USD</span>
+            <>
+                <form id="hola" action=''>
+                    <label htmlFor="">Name: </label>
+                    <input type="text" placeholder="Name" onChange={e => this.changeState('name' , e)} />
+                    <label htmlFor="">Email: </label>
+                    <input type="Email" placeholder="Email" onChange={e => this.changeState('email' , e)} />
+                    <input type="submit" value="Submit"/>
+                </form>
+                <div className="">
+                    <h2>Name: {this.state.name}</h2>
+                    <h4>Email: {this.state.email}</h4>
                 </div>
-            </article>
+                <h2>{this.state.date}</h2>
+            </>
         )
     }  
+
+    componentDidMount() {
+        let el = document.getElementById('hola');
+        console.log(el)
+        this.clockInterval = setInterval(this.clockSeconds, 1000)
+    }
+
+    componentDidUpdate() {
+        console.log('hello')
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockInterval)
+    }
 
 }
 
